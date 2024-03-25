0082a2ff05cf6be0913df1f02f334ea064492542
programmermame <mameatpickout@gmail.com>
2024-03-25 21:52:32 +0300

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,58 +1,25 @@
 #!/usr/bin/python3
-
-"""
-
-Returns to-do list information for a given employee ID.
-
-
-This script takes an employee ID as a command-line argument and fetches
-
-the corresponding user information and to-do list from the JSONPlaceholder API.
-
-It then prints the tasks completed by the employee.
-
-"""
-
-
+"""Script that uses REST API"""
 import requests
-
 import sys
 
-
-
 if __name__ == "__main__":
+    if len(sys.argv) == 2 and sys.argv[1].isdigit():
+        args = {"id": sys.argv[1]}
+        users = requests.get("https://jsonplaceholder.typicode.com/users",
+                             params=args).json()
+        args = {"userId": sys.argv[1]}
+        todos = requests.get("https://jsonplaceholder.typicode.com/todos",
+                             params=args).json()
+        todos_len = 0
+        todos_arr = []
+        for i in todos:
+            if i.get("completed"):
+                todos_arr.append(i)
+                todos_len += 1
 
-    # Base URL for the JSONPlaceholder API
+        print("Employee {} is done with tasks({}/{}):".format(
+              users[0].get("name"), todos_len, len(todos)))
 
-    url = "https://jsonplaceholder.typicode.com/"
-
-
-    # Get the employee information using the provided employee ID
-
-    employee_id = sys.argv[1]
-
-    user = requests.get(url + "users/{}".format(employee_id)).json()
-
-
-    # Get the to-do list for the employee using the provided employee ID
-
-    params = {"userId": employee_id}
-
-    todos = requests.get(url + "todos", params).json()
-
-
-    # Filter completed tasks and count them
-
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-
-
-    # Print the employee's name and the number of completed tasks
-
-    print("Employee {} is done with tasks({}/{}):".format(
-
-        user.get("name"), len(completed), len(todos)))
-
-
-    # Print the completed tasks one by one with indentation
-
-    [print("\t {}".format(complete)) for complete in completed]
+        for i in todos_arr:
+            print("\t {}".format(i.get("title")))
